ac370fc526af785701924e4b0446c028af9a95bf
jerrard suell <seulljay@gmail.com>
2019-11-06 20:37:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  name = "Arel"
+  puts "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  array = [attendees]
+  array.each do |attendees|
+    puts "Hello, my name is #{attendees}."
+    "Hello, my name is #{attendees}."
+  end
+end
